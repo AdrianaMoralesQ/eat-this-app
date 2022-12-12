acfab3f6302997a00f7bfff7290b3cf3bd765007
Adriana Morales <amorales25@gmail.com>
2022-12-12 15:05:01 -0500

    added working data from airtable (2 types)

--- Styled Components.js
+++ Styled Components.js
@@ -4,8 +4,3 @@ export const Wrapper = styled.div`
 	display: flex;
 	margin-top: 20vh;
 `;
-
-export const TypeWrapper = styled.div`
-	background-color: hsl(260, 100%, 80%);
-	outline: solid hsl(260, 100%, 80%) 0.25rem;
-`;
