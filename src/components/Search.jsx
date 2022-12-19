8b425360ddfac0188376bcd15017ca3f4aa34c59
Adriana Morales <amorales25@gmail.com>
2022-12-19 12:00:12 -0500

    changed router from /test

---
+++ src/components/Search.jsx
@@ -0,0 +1,27 @@
+const SearchCategories = () => {
+	// create form input
+	// map over ingredient.name
+	// return
+	// const usersFilteredByInput = users.filter((user) =>
+	// user.name.toLowerCase().includes(formValue.members.toLowerCase())
+	// );
+	// <label>
+	// 		<input
+	// 			name="members"
+	// 			placeholder="Start typing another user's name"
+	// 			type="text"
+	// 			onChange={handleChange}
+	// 			value={members}
+	// 		/>
+	// 		<DropdownWrapper>
+	// 			{usersFilteredByInput.map((user, i) => (
+	// 				<Autocomplete
+	// 					name={user.name}
+	// 					userInput={formValue.members}
+	// 					onClickEvent={handleSelect}
+	// 					id={user._id}
+	// 				/>
+	// 			))}
+	// 		</DropdownWrapper>
+	// 	</label>;
+};
